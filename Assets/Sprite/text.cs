43ac24f776a5eb38d9cffe874b833590d268927c
Nak3nf <1828741676@qq.com>
2022-01-12 19:11:56 +0800

    第一次测试

---
+++ Assets/Sprite/text.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text : MonoBehaviour
+{
+    public bool textt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textt = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
