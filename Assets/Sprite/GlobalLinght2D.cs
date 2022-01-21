9ad21dc349ccea9f2a72b29d0b73d8804572735f
Nak3nf <1828741676@qq.com>
2022-01-21 18:15:41 +0800

    添加了白天黑夜效果

---
+++ Assets/Sprite/GlobalLinght2D.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+public class GlobalLinght2D : MonoBehaviour
+{
+    Light2D San;//获取灯光组件
+    public float SunTime;//太阳交替的时间
+    public bool isSun=true;//是否执行交替
+    public float WhiteSeep;//白天到晚上的速度
+    public float BlackSeep;//晚上到白天的速度
+    public float BrightGameTime = 120;//游戏有日光的时间
+    
+
+
+    void Start()
+    {
+        San = GetComponent<Light2D>();
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+        if (BrightGameTime<=0)//结束代码
+        {
+            San.intensity *= 0.9f;
+            return;
+        }
+        BrightGameTime -= Time.deltaTime;
+
+        if (isSun)
+        {
+            whiteblack();
+        }
+        if (!isSun)
+        {
+            blackwhite();
+        }
+
+
+        
+    }
+    void whiteblack()//白天到晚上
+    {
+ 
+         San.intensity *= WhiteSeep; 
+        SunTime -= Time.deltaTime;
+        if (SunTime <= 0)
+        {
+            Debug.Log(string.Format("白天", Time.time));
+            isSun = false;
+            SunTime = 10.0f;
+        }
+    }
+    void blackwhite()//晚上到白天
+    {
+        San.intensity *= BlackSeep;
+        SunTime -= Time.deltaTime;
+        if (SunTime <= 0)
+        {
+            Debug.Log(string.Format("晚上", Time.time));
+            isSun = true;
+            SunTime = 10.0f;
+        }
+    }
+    
+}
